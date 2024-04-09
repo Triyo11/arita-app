e388a6aea3dd950ccecffd4d3899ba6b64325b3a
Triyo Krismantoro <rusdiantoro117@gmail.com>
2024-04-09 16:01:16 +0700

    add animation in catalog news page and fix responsibilities

--- src/components/NewsCard/index.jsx
+++ src/components/NewsCard/index.jsx
@@ -3,27 +3,27 @@ import "animate.css";
 
 const NewsCard = ({ news }) => {
   return (
-    <div className="flex flex-wrap justify-evenly p-4 gap-8">
+    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 items-center p-4 gap-8">
       {news.map((item, index) => {
         let pubDate = new Date(item.pubDate);
         pubDate = pubDate.toLocaleString("en-GB");
         return (
           <div
             key={item.link}
-            className="md:w-80 w-40 relative animate__animated animate__zoomIn"
+            className="w-full h-full flex flex-col items-start justify-start animate__animated animate__zoomIn"
           >
             <Image
               src={item.thumbnail}
               alt="gambar error"
-              width={350}
               height={350}
-              className="rounded-md"
+              width={350}
+              className="rounded-md w-full max-h-max"
             />
-            <div className="flex flex-col justify-start items-start py-4">
+            <div className="py-4">
               <p>{pubDate}</p>
               <a
                 href={item.link}
-                className="text-lg font-semibold focus:text-color-accent2"
+                className="lg:text-lg md:text-base text-sm font-semibold focus:text-color-accent2"
               >
                 {item.title}
               </a>
