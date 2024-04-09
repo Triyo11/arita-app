5be9d9db0b0723342e3b4a8008a74e19d95d7291
Triyo Krismantoro <rusdiantoro117@gmail.com>
2024-04-09 16:02:37 +0700

    fix font size header catalog news page responsibilities

--- src/components/NewsCard/Header.jsx
+++ src/components/NewsCard/Header.jsx
@@ -12,14 +12,21 @@ const HeaderNewsCard = ({ headerTitle, source }) => {
     const mediumScreen = window.matchMedia("(min-width: 768px)").matches;
     if (mediumScreen) {
       setIsMediumScreen(true);
-    }  
-  }, [])
+    }
+  }, []);
 
   return (
     <header className="px-20 sticky top-14 z-10 bg-color-primary">
       <div className="md:px-8 md:py-2 p-4">
-        <Link href={`/channel/${source}`}><ArrowUUpLeft size={48} className={`text-color-primary bg-color-secondary rounded-3xl p-2 fixed bottom-10 right-10 z-10 ${isMediumScreen ? "hidden" : "block"}`}/></Link>
-        <h2 className="flex justify-center font-bold md:text-3xl text-2xl text-color-secondary">
+        <Link href={`/channel/${source}`}>
+          <ArrowUUpLeft
+            size={48}
+            className={`text-color-primary bg-color-secondary rounded-3xl p-2 fixed bottom-10 right-10 z-10 ${
+              isMediumScreen ? "hidden" : "block"
+            }`}
+          />
+        </Link>
+        <h2 className="flex justify-center font-bold md:text-3xl sm:text-2xl text-md text-color-secondary">
           {headerTitle}
         </h2>
       </div>
