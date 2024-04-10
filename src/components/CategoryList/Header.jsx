9ad6b4ecca2f9c201724b8ebb10138c3cdbc190d
Triyo Krismantoro <rusdiantoro117@gmail.com>
2024-04-11 06:11:33 +0700

    remove backButton component

--- src/components/CategoryList/Header.jsx
+++ src/components/CategoryList/Header.jsx
@@ -1,11 +1,6 @@
-"use client";
-
-import BackButton from "../micro/BackButton";
-
 const HeaderCategoryList = ({ name }) => {
   return (
     <div className="md:py-8 py-4 md:px-32 px-6">
-      <BackButton />
       <div className="text-center font-semibold md:text-3xl text-lg flex md:flex-row md:gap-2` flex-col justify-center text-color-primary">
         <div>Kategori Berita</div>
         <div>"{name.toUpperCase()}"</div>
--- src/components/NewsCard/Header.jsx
+++ src/components/NewsCard/Header.jsx
@@ -1,10 +1,7 @@
-import BackButton from "../micro/BackButton";
-
 const HeaderNewsCard = ({ headerTitle, source }) => {
   return (
     <header className="px-20 sticky top-14 z-10 bg-color-primary">
       <div className="md:px-8 md:py-2 p-4">
-        <BackButton />
         <h2 className="flex justify-center font-bold md:text-3xl sm:text-2xl text-md text-color-secondary">
           {headerTitle}
         </h2>
