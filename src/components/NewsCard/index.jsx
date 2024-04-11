6bd3f35d2d469b93915b4f819102b407b0ad8fd8
Triyo Krismantoro <rusdiantoro117@gmail.com>
2024-04-11 13:36:37 +0700

    fix respons catalog news page on mobile

--- src/components/NewsCard/index.jsx
+++ src/components/NewsCard/index.jsx
@@ -3,7 +3,7 @@ import "animate.css";
 
 const NewsCard = ({ news }) => {
   return (
-    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 items-center p-4 gap-8">
+    <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-center p-4 gap-8">
       {news.map((item, index) => {
         let pubDate = new Date(item.pubDate);
         pubDate = pubDate.toLocaleString("en-GB");
