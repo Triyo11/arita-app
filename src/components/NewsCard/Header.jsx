659fb10295fadc4fdede4cc595dd9e46eba91cd7
Triyo Krismantoro <rusdiantoro117@gmail.com>
2024-04-16 05:37:37 +0700

    fix header news catalog page

--- src/components/NewsCard/Header.jsx
+++ src/components/NewsCard/Header.jsx
@@ -1,6 +1,6 @@
 const HeaderNewsCard = ({ headerTitle, source }) => {
   return (
-    <header className="px-20 sticky top-14 z-10 bg-color-primary">
+    <header className="px-20 sticky top-14 z-10 bg-secondary">
       <div className="md:px-8 md:py-2 p-4">
         <h2 className="flex justify-center font-bold md:text-3xl sm:text-2xl text-md text-color-secondary">
           {headerTitle}
