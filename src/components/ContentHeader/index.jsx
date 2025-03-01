76ed6efc3292a4ef25ee49c3a749b2e04a7d711d
Triyo Krismantoro <rusdiantoro117@gmail.com>
2025-03-01 10:11:28 +0700

    dev: recreate content header

--- src/components/ContentHeader/index.jsx
+++ src/components/ContentHeader/index.jsx
@@ -1,13 +1,13 @@
-const HeaderNewsCard = ({ headerTitle, source }) => {
+const ContentHeader = ({ title }) => {
   return (
     <header className="px-20 sticky top-14 z-10 bg-secondary">
       <div className="md:px-8 md:py-2 p-4">
         <h2 className="flex justify-center font-bold md:text-3xl sm:text-2xl text-md text-color-secondary">
-          {headerTitle}
+          {title}
         </h2>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default HeaderNewsCard;
+export default ContentHeader;
